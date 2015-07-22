a1ae6959ae9babf73b76e4285ee09971bce54886
Sam Curley <CurleySamuel@gmail.com>
2015-07-22 11:23:42 -0700

    Correctly parse returned region info.

--- region/region_info.py
+++ region/region_info.py
@@ -1,7 +1,27 @@
+from struct import unpack
+from pb.HBase_pb2 import RegionInfo as pbRegionInfo
+
+
 class RegionInfo:
-    pass
+
+    def __init__(self, table, name, start, stop):
+        self.table = table
+        self.region_name = name
+        self.start_key = start
+        self.stop_key = stop
 
 
 def region_info_from_cell(cell):
-    return RegionInfo()
+    magic = unpack(">I", cell.value[:4])[0]
+    # 4 bytes: PBUF
+    if magic != 1346524486:
+        # Either it's a corrupt message or an unsupported region info version.
+        return 1 / 0  # TODO
+    region_info = pbRegionInfo()
+    region_info.ParseFromString(cell.value[4:-4])
+    table = region_info.table_name.qualifier
+    region_name = cell.row
+    start_key = region_info.start_key
+    stop_key = region_info.end_key
+    return RegionInfo(table, region_name, start_key, stop_key)
 
